7303300c20a80b8b58e902e3a11aaa73168581a2
mohammad jahangir <mohammad.jahangir@lntinfotech.com>
2016-03-23 04:59:59 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <body bgcolor="#FFF66C">
+  <h1> Version 1.0 <h1>
+  <h2> Sample PHP App <h2>
+  <h2> Deployed with Capistrano <h2>
+    <?php
+    echo "<h4>Current date and time => " . date("r") . "</h4>";
+    ?>
+    </body>
+</html>
+
